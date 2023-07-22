b9ee4e47acce2e994df0331162b6c2ce97d97eab
kksgh <kochetkovaksenia1@gmail.com>
2023-07-22 17:30:10 +0300

    коммит

--- lesson7_treasure.py
+++ lesson7_treasure.py
@@ -2,28 +2,18 @@ from selenium import webdriver
 import time
 import math
 
-link = "http://suninjuly.github.io/get_attribute.html"
-
 try:
     browser = webdriver.Chrome()
-    browser.get(link)
+    browser.get("http://suninjuly.github.io/get_attribute.html")
     im = browser.find_element(By.ID, "treasure")
     x = im.get_attribute("valuex")
     def calc(x):
         return str(math.log(abs(12*math.sin(int(x)))))
     y = calc(x)
-    anws = browser.find_element(By.CSS_SELECTOR, "#answer")
-    anws.send_keys(y)
-
-    option1 = browser.find_element(By.CSS_SELECTOR, "[type = 'checkbox']")
-    option1.click()
-
-    option2 = browser.find_element(By.CSS_SELECTOR, "[value = 'robots']")
-    option2.click()
-
-    button = browser.find_element(By.CSS_SELECTOR, "[type = 'submit']")
-    button.click()
-
+    anws = browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(y)
+    option1 = browser.find_element(By.CSS_SELECTOR, "[type = 'checkbox']").click()
+    option2 = browser.find_element(By.CSS_SELECTOR, "[value = 'robots']").click()
+    button = browser.find_element(By.CSS_SELECTOR, "[type = 'submit']").click()
 finally:
     time.sleep(5)
     browser.quit()
